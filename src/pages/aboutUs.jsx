e78e811260294fdca8c56d3bc811b8fad3684cfd
James olujae <96151013+Jamesolujae123@users.noreply.github.com>
2024-07-08 15:37:27 +0100

    Add files via upload

--- src/pages/aboutUs.jsx
+++ src/pages/aboutUs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useContext } from "react";
 import { AppContext, Welcome, UserL } from "../AppContext";
+import "./AboutUs.css";
 
 const AboutUs = () => {
   const welc = useContext(Welcome);
@@ -20,10 +21,104 @@ const AboutUs = () => {
       {" "}
       <div className="intro-2">
         <div className="abt">
-          <span className="acc-3">About us</span>
-          <p className="tng">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
-          </p>
+          <span className="acct-33">About us</span>
+          <p className="tng-g">About Recipe net</p>
+        </div>
+      </div>
+      <div className="about-things">
+        <div className="Head">
+          {" "}
+          <h2>Welcome to Recipe Net!</h2>
+        </div>
+        <div className="Head">
+          <span>
+            At Recipe Net, we believe that cooking is more than just preparing
+            meals—it's a journey of discovery, creativity, and joy. Our mission
+            is to inspire and empower home cooks of all levels to create
+            delicious and nutritious meals for themselves and their loved ones.
+          </span>
+        </div>
+        <div className="who-cont">
+          <h2>Who We Are</h2>
+          <span>
+            Recipe Net is a community-driven platform dedicated to sharing the
+            best food recipes and food-related posts. Our team of passionate
+            food enthusiasts, professional chefs, and nutrition experts work
+            tirelessly to bring you a diverse range of recipes from around the
+            world. Whether you're a beginner looking for simple, quick meals or
+            an experienced cook seeking new challenges, Recipe Net has something
+            for everyone.
+          </span>
+        </div>
+        <div className="Mission-cont">
+          <h2>Our Mission</h2>
+          <span>Our mission is to:</span>
+          <ul>
+            <li>
+              Inspire Creativity: We provide a wide variety of recipes that
+              encourage creativity and experimentation in the kitchen. From
+              traditional dishes to modern twists, we aim to spark your culinary
+              imagination.
+            </li>
+            <li>
+              Promote Healthy Eating: We believe that good food is the
+              foundation of a healthy lifestyle. Our recipes are crafted with
+              quality ingredients and balanced nutrition in mind, ensuring that
+              you and your family can enjoy wholesome, delicious meals.
+            </li>
+            <li>
+              Foster Community: Cooking is a communal activity, and we strive to
+              build a supportive community where food lovers can connect, share,
+              and learn from each other. Our platform allows users to share
+              their own recipes, tips, and food experiences, fostering a rich
+              and interactive culinary network.
+            </li>
+          </ul>
+        </div>
+        <div className="offer-cont">
+          <h2>What We Offer</h2>
+          <ol>
+            <li>
+              Extensive Recipe Collection: Browse through our extensive library
+              of recipes, ranging from appetizers and main courses to desserts
+              and snacks. Each recipe is meticulously tested and reviewed to
+              guarantee its accuracy and taste.
+            </li>
+            <li>
+              Food Blogs and Articles: Stay updated with the latest food trends,
+              cooking tips, and culinary news through our engaging blog posts
+              and articles. Our content is crafted to educate, entertain, and
+              inspire food enthusiasts.
+            </li>
+            <li>
+              {" "}
+              Interactive Community: Join our vibrant community of food lovers.
+              Share your own recipes, comment on posts, and exchange ideas with
+              fellow home cooks. At Recipe Net, every member's contribution is
+              valued and celebrated.
+            </li>
+            <li>
+              Step-by-Step Guides: Our detailed step-by-step guides and video
+              tutorials make cooking accessible to everyone. Whether you're
+              mastering a complex dish or learning a basic cooking technique,
+              we've got you covered.
+            </li>
+          </ol>
+        </div>
+        <div className="Join-cont">
+          <h2>Join Us on Our Culinary Journey</h2>
+          <span>
+            At Recipe Net, we believe that every meal has a story, and every
+            recipe is a chance to create lasting memories. Join us on our
+            culinary journey and discover the joys of cooking. Explore new
+            flavors, improve your cooking skills, and connect with a community
+            that shares your passion for food.
+          </span>
+
+          <span>
+            Thank you for being a part of Recipe Net. Together, let's make every
+            meal a masterpiece.
+          </span>
         </div>
       </div>
     </div>
--- src/pages/contactUs.jsx
+++ src/pages/contactUs.jsx
@@ -23,9 +23,128 @@ const Contact = () => {
       <div className="intro-2">
         <div className="abt">
           <span className="acc-3">Contact us</span>
-          <p className="tng">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+          <p className="tng">Get in Touch with Recipe Net</p>
+        </div>
+      </div>
+      <div className="contact-us-cont">
+        <div className="contact-head">
+          <h2>Contact Us</h2>
+          <h3>Get in Touch with Recipe Net</h3>
+          <span>
+            We'd love to hear from you! Whether you have a question, feedback,
+            or simply want to share your culinary experiences, the Recipe Net
+            team is here to connect with you. Your thoughts and suggestions are
+            invaluable to us as we strive to improve our platform and better
+            serve our community of food enthusiasts.
+          </span>
+        </div>
+        <div className="reach-cont">
+          <h2>How to Reach Us</h2>
+          <h3>Email:</h3>
+          <span>
+            For general inquiries, recipe submissions, or support, please email
+            us at: contact@recipenetwork.com
+          </span>
+        </div>
+        <div className="social-cont">
+          <h2>Social Media:</h2>
+          <span>
+            Connect with us on our social media platforms for the latest
+            updates, cooking tips, and food inspiration:
+          </span>
+          <div className="soc-arr">
+            <div className="soc">
+              <span className="soc-i">Facebook:</span>
+              <span>facebook.com/recipenetwork</span>
+            </div>
+            <div className="soc">
+              <span className="soc-i">Twitter:</span>
+              <span>twitter.com/recipenetwork</span>
+            </div>
+            <div className="soc">
+              <span className="soc-i">Instagram:</span>
+              <span>instagram.com/recipenetwork</span>
+            </div>
+            <div className="soc">
+              <span className="soc-i">Pinterest:</span>
+              <span>pinterest.com/recipenetwork</span>
+            </div>
+          </div>
+        </div>
+        <div className="Address-cont">
+          <h2>Mailing Address:</h2>
+          <span>
+            If you prefer to reach us by mail, you can send your correspondence
+            to:
+          </span>
+          <div>
+            <p>Recipe Net</p>
+            <p>123 Culinary Avenue</p>
+            <p>Gourmet City, FL 54321</p>
+          </div>
+        </div>
+        <div className="Phone-cont">
+          <h2>Phone:</h2>
+          <span>
+            For urgent matters, feel free to call us at: (123) 456-7890
+          </span>
+          <p>
+            Our phone lines are open Monday to Friday, from 9:00 AM to 5:00 PM
+            (EST).
           </p>
+        </div>
+        <div className="Feeddss-cont">
+          <div>
+            <h2>Feedback and Suggestions</h2>
+            <span>
+              Your feedback helps us grow and improve. If you have any
+              suggestions on how we can enhance your experience on Recipe Net,
+              please don't hesitate to let us know. We're always eager to hear
+              your thoughts and implement changes that benefit our community.
+            </span>
+          </div>
+          <div>
+            <h2>Recipe Submissions</h2>
+            <span>
+              Do you have a delicious recipe that you'd like to share with the
+              world? Submit your recipes to us, and you might see them featured
+              on Recipe Net! Email your recipes, along with any photos and a
+              brief description, to recipes@recipenetwork.com.
+            </span>
+          </div>
+          <div>
+            <h2>Collaborations and Partnerships</h2>
+            <span>
+              We're open to collaborations with brands, influencers, and fellow
+              food enthusiasts. If you're interested in partnering with Recipe
+              Net for special projects, events, or content creation, please
+              reach out to us at partnerships@recipenetwork.com.
+            </span>
+          </div>
+          <div>
+            <h2>Technical Support</h2>
+            <span>
+              If you're experiencing any technical issues with our website or
+              have questions about using our platform, our support team is here
+              to help. Please email us at support@recipenetwork.com, and we'll
+              assist you as soon as possible.
+            </span>
+          </div>
+          <div>
+            <h2>Join Our Community</h2>
+            <span>
+              Stay connected and be part of our growing community by signing up
+              for our newsletter. Receive the latest recipes, cooking tips, and
+              exclusive content straight to your inbox. Subscribe here.
+            </span>
+          </div>
+        </div>
+        <div className="Thanks-cont">
+          <span>
+            Thank you for visiting Recipe Net! We're excited to connect with you
+            and share our love for food and cooking. Your support and engagement
+            make our community vibrant and inspiring.
+          </span>
         </div>
       </div>
     </div>
