ef4657244e09e254075e2812b463f5b7a7c23460
James olujae <96151013+Jamesolujae123@users.noreply.github.com>
2024-07-08 07:35:36 -0700

    Update AddBlog.jsx

--- src/pages/AddBlog.jsx
+++ src/pages/AddBlog.jsx
@@ -1,128 +1,144 @@
 import React from "react";
+import "./SubmitRecipe.css";
+import { useState, useEffect } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import ReactHtmlParser from "@orrisroot/react-html-parser";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Toaster, toast } from "sonner";
+import "./AddBlog.css";
 import { useContext } from "react";
-import { AppContext, Welcome, UserL } from "../AppContext";
-import "./AboutUs.css";
+import { Welcome, AppContext, UserL } from "../AppContext";
+import config from "../../config";
 
-const AboutUs = () => {
+const AddBlog = () => {
+  const [repValue, setRepValue] = useState({ image: "" });
+  const [image, setImage] = useState(null);
+
+  const token = localStorage.getItem("token");
+
+  const userLastName = localStorage.getItem("user_lastName");
+
+  const baseUrl = "https://recp-backend.onrender.com";
+
   const welc = useContext(Welcome);
   const data = useContext(AppContext);
   const usr = useContext(UserL);
 
-  const token = localStorage.getItem("token");
-  const userLastName = localStorage.getItem("user_lastName");
+  const handleImage = (e) => {
+    const { name, value } = e.target;
+    setRepValue((prevVal) => ({ ...prevVal, [name]: value }));
+    setImage(e.target.files[0]);
+  };
 
   if (token) {
     welc.setWelcome(true);
     data.setIsLoggedin(true);
     usr.setUserLN(userLastName);
   }
+
+  const navigateTo = useNavigate();
+
+  const headers = { Authorization: `Bearer ${token} ` };
+
+  const handleSub = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData();
+      formData.append("title", `${blogValues.title}`);
+      formData.append("description", `${blogValues.description}`);
+      formData.append("image", image);
+      console.log(formData);
+      console.log(blogValues.title);
+      console.log(blogValues.description);
+
+      const response = await axios.post(`${config.baseUrl}/posts`, formData, {
+        headers,
+      });
+
+      console.log(response);
+
+      console.log(data);
+
+      if (response) {
+        setTimeout(() => {
+          toast.success("Post sent successfully");
+        }, 1000);
+        navigateTo("/Blog");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setblogValues((prevVal) => ({ ...prevVal, [name]: value }));
+    setImage(e.target.files[0]);
+  };
+
+  const [blogValues, setblogValues] = useState({
+    title: "",
+    description: "",
+  });
   return (
-    <div>
-      {" "}
+    <div className="add-cont">
+      <Toaster />
       <div className="intro-2">
         <div className="abt">
-          <span className="acct-33">About us</span>
-          <p className="tng-g">About Recipe net</p>
+          <span className="acc-3">Make a blog post</span>
+          <p className="tng">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+          </p>
         </div>
       </div>
-      <div className="about-things">
-        <div className="Head">
-          {" "}
-          <h2>Welcome to Recipe Net!</h2>
+      <form onSubmit={handleSub} className="fom">
+        <div className="title">
+          <div className="blog-t">
+            <div className="blog-tt">
+              <label className="tingss">Title</label>
+              <input
+                name="title"
+                onChange={handleChange}
+                className="bb-name"
+                type="text"
+                value={blogValues.title}
+              />
+            </div>
+          </div>
+          <div className="b-dd">
+            <label className="tingss" htmlFor="">
+              Content
+            </label>
+            <textarea
+              cols="30"
+              rows="5"
+              onChange={handleChange}
+              className="bb-d-name"
+              name="description"
+              type="text"
+              value={blogValues.description}
+            ></textarea>
+          </div>
         </div>
-        <div className="Head">
-          <span>
-            At Recipe Net, we believe that cooking is more than just preparing
-            meals—it's a journey of discovery, creativity, and joy. Our mission
-            is to inspire and empower home cooks of all levels to create
-            delicious and nutritious meals for themselves and their loved ones.
-          </span>
+        <div className="food-imgr">
+          <input
+            className=""
+            type="file"
+            name="image"
+            onChange={handleImage}
+            accept="image/*"
+            value={repValue.image}
+          />
+          <label htmlFor="">Add an image size: h= 420px by w=600px </label>
         </div>
-        <div className="who-cont">
-          <h2>Who We Are</h2>
-          <span>
-            Recipe Net is a community-driven platform dedicated to sharing the
-            best food recipes and food-related posts. Our team of passionate
-            food enthusiasts, professional chefs, and nutrition experts work
-            tirelessly to bring you a diverse range of recipes from around the
-            world. Whether you're a beginner looking for simple, quick meals or
-            an experienced cook seeking new challenges, Recipe Net has something
-            for everyone.
-          </span>
-        </div>
-        <div className="Mission-cont">
-          <h2>Our Mission</h2>
-          <span>Our mission is to:</span>
-          <ul>
-            <li>
-              Inspire Creativity: We provide a wide variety of recipes that
-              encourage creativity and experimentation in the kitchen. From
-              traditional dishes to modern twists, we aim to spark your culinary
-              imagination.
-            </li>
-            <li>
-              Promote Healthy Eating: We believe that good food is the
-              foundation of a healthy lifestyle. Our recipes are crafted with
-              quality ingredients and balanced nutrition in mind, ensuring that
-              you and your family can enjoy wholesome, delicious meals.
-            </li>
-            <li>
-              Foster Community: Cooking is a communal activity, and we strive to
-              build a supportive community where food lovers can connect, share,
-              and learn from each other. Our platform allows users to share
-              their own recipes, tips, and food experiences, fostering a rich
-              and interactive culinary network.
-            </li>
-          </ul>
-        </div>
-        <div className="offer-cont">
-          <h2>What We Offer</h2>
-          <ol>
-            <li>
-              Extensive Recipe Collection: Browse through our extensive library
-              of recipes, ranging from appetizers and main courses to desserts
-              and snacks. Each recipe is meticulously tested and reviewed to
-              guarantee its accuracy and taste.
-            </li>
-            <li>
-              Food Blogs and Articles: Stay updated with the latest food trends,
-              cooking tips, and culinary news through our engaging blog posts
-              and articles. Our content is crafted to educate, entertain, and
-              inspire food enthusiasts.
-            </li>
-            <li>
-              {" "}
-              Interactive Community: Join our vibrant community of food lovers.
-              Share your own recipes, comment on posts, and exchange ideas with
-              fellow home cooks. At Recipe Net, every member's contribution is
-              valued and celebrated.
-            </li>
-            <li>
-              Step-by-Step Guides: Our detailed step-by-step guides and video
-              tutorials make cooking accessible to everyone. Whether you're
-              mastering a complex dish or learning a basic cooking technique,
-              we've got you covered.
-            </li>
-          </ol>
-        </div>
-        <div className="Join-cont">
-          <h2>Join Us on Our Culinary Journey</h2>
-          <span>
-            At Recipe Net, we believe that every meal has a story, and every
-            recipe is a chance to create lasting memories. Join us on our
-            culinary journey and discover the joys of cooking. Explore new
-            flavors, improve your cooking skills, and connect with a community
-            that shares your passion for food.
-          </span>
 
-          <span>
-            Thank you for being a part of Recipe Net. Together, let's make every
-            meal a masterpiece.
-          </span>
-        </div>
-      </div>
+        <button className="sb">Submit</button>
+      </form>
     </div>
   );
 };
 
-export default AboutUs;
+export default AddBlog;
