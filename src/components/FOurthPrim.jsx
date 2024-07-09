eb58cbac241e7a66c03dbc30e84c04c2028ad4c1
James olujae <96151013+Jamesolujae123@users.noreply.github.com>
2024-07-09 08:00:50 -0700

    Update FOurthPrim.jsx

--- src/components/FOurthPrim.jsx
+++ src/components/FOurthPrim.jsx
@@ -19,6 +19,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { FaArrowUp } from "react-icons/fa";
 import config from "../../config";
+import Placeholder from "../assets/Placeholder-web.jpg";
 
 const FourthPrim = ({ topting, heading }) => {
   const navigateTo = useNavigate();
@@ -90,7 +91,11 @@ const FourthPrim = ({ topting, heading }) => {
                 <div className="wrapper">
                   <img
                     className="food-img"
-                    src={`${config.baseUrl}${food.image_url}`}
+                    src={
+                      !food.image_url
+                        ? `${Placeholder}`
+                        : `${config.baseUrl}${food.image_url}`
+                    }
                     alt=""
                   />
                 </div>
@@ -98,55 +103,74 @@ const FourthPrim = ({ topting, heading }) => {
             ))}
           </div>
         </div>
-        <div className="view-more">
-          {more ? (
-            <div className="remainder-cont">
-              <div className="sec-m">
-                {foods?.slice(3).map((food) => (
-                  <div
-                    key={food}
-                    onClick={() => {
-                      navigateTo(`/recipe-d/${food.id}`);
-                    }}
-                    className="food"
-                  >
-                    <div className="food-d">
-                      <div className="ting">
-                        <p className="f-name">{food.food_name}</p>
-                        <div className="spatu">
-                          {" "}
-                          <span className="spatula">
-                            <TbGrillSpatula />
-                            <TbGrillSpatula />
-                            <TbGrillSpatula />
-                            <TbGrillSpatula />
-                            <TbGrillSpatula className="fade" />
-                          </span>
+        {foods?.length > 3 ? (
+          <div className="view-more">
+            {more ? (
+              <div className="remainder-cont">
+                <div
+                  className="sec-m"
+                  data-aos="fade-left"
+                  data-aos-once="true"
+                >
+                  {foods?.slice(3).map((food) => (
+                    <div
+                      onClick={() => {
+                        navigateTo(`/recipe-d/${food.id}`);
+                      }}
+                      key={food}
+                      className="food"
+                    >
+                      <div className="food-d">
+                        <div className="ting">
+                          <p className="f-name">{food.food_name}</p>
+                          <div className="spatu">
+                            {" "}
+                            <span className="spatula">
+                              <TbGrillSpatula />
+                              <TbGrillSpatula />
+                              <TbGrillSpatula />
+                              <TbGrillSpatula />
+                              <TbGrillSpatula className="fade" />
+                            </span>
+                          </div>
+                        </div>
+                        <div className="Auth">
+                          <p className="Author">
+                            {food.User.first_name} {food.User.last_name}
+                          </p>
                         </div>
                       </div>
-                      <div className="Auth">
-                        <p className="Author">
-                          {food.User.first_name} {food.User.last_name}
-                        </p>
+                      <div className="wrapper">
+                        <img
+                          className="food-img"
+                          src={
+                            !food.image_url
+                              ? `${Placeholder}`
+                              : `${config.baseUrl}${food.image_url}`
+                          }
+                          alt=""
+                        />
                       </div>
                     </div>
+                  ))}
+                </div>
 
-                    <div className="wrapper">
-                      <img className="food-img" src={`${config.baseUrl}${food.image_url}`} alt="" />
-                    </div>
-                  </div>
-                ))}
+                <span className="vw-cont-up" onClick={closeRemainder}>
+                  <FaArrowUp />
+                </span>
               </div>
-              <span className="vw-cont-up" onClick={closeRemainder}>
-                <FaArrowUp />
+            ) : (
+              <span onClick={displayRemainder} className="vw-cont">
+                view more{" "}
+                <span>
+                  <FaArrowDown />
+                </span>
               </span>
-            </div>
-          ) : (
-            <span onClick={displayRemainder} className="vw-cont">
-              view more <FaArrowDown />
-            </span>
-          )}
-        </div>
+            )}
+          </div>
+        ) : (
+          ""
+        )}{" "}
       </div>
     </div>
   );
